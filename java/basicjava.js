2ede8b9e576c243657bd33e8d256722a7df4fd21
Dheenathayalan007 <thayalandheena511@gmail.com>
2025-01-30 13:46:15 +0530

    Add files via upload

---
+++ java/basicjava.js
@@ -0,0 +1,17 @@
+const Name="Dheena";
+console.log("Name:",Name)
+
+const Dep="Physics";
+console.log("Dep:",Dep)
+
+const Number=9944770000;
+console.log("ph no:",Number)
+
+const collage="AVC";
+console.log("collage:",collage)
+
+const Address="Dheena,\t\n Aathukidi, \t\n mayiladuthuray";
+console.log("Address:",Address)
+
+const school="GHSS";
+console.log("school:",school)
---
+++ java/variable.js
@@ -0,0 +1,18 @@
+let dheena="dheaen atha yalan";
+const d="thaya";
+console.log("is a wonderful"+dheena)
+console.log(dheena.slice(5,6))
+console.log(dheena.includes("dheina"))
+console.log(dheena.split(" "))
+
+a=23340000000000000000000000000000000000000000000
+console.log(typeof(a))
+
+
+let variable="100.12345678";
+console.log(typeof(dheena))
+
+console.log(isNaN("1")) // check it is not a  nimber
+ 
+a2=12.5
+console.log(Number.isInteger(a2))
